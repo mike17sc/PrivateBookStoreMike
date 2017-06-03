b6959f070684ecfb7444b49d4a6a358efc0bb9b8
mike17sc <mike17sc@hotmail.com>
2017-06-03 10:43:57 +0200

    Init

---
+++ src/main/java/com/training/PrivateBookStoreMikeApplication.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateBookStoreMikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateBookStoreMikeApplication.class, args);
+	}
+}
